1fb128d6c176cf633c88812b9324d4db7fd54a7c
ibrohim0117 <ibrohim.dev.uz@gmail.com>
2024-11-15 12:29:58 +0500

    start product app

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from product.models import Product, Category, SubCategory, Tag
+
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Tag)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,62 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+from django.utils.text import slugify
+
+from user.models import BaseCreatedModel
+
+
+class Category(BaseCreatedModel):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True, blank=True, editable=False)
+
+    def save(self, *args, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.slug = slugify(self.name)
+        while Category.objects.filter(slug=self.slug).exists():
+            self.slug += '-' + str(uuid.uuid4()).split()[-1]
+        super().save(*args, force_insert=force_insert, force_update=force_update, using=using,
+                     update_fields=update_fields)
+
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(BaseCreatedModel):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='sub_categories')
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True, blank=True, editable=False)
+
+    def save(self, *args, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.slug = slugify(self.name)
+        while SubCategory.objects.filter(slug=self.slug).exists():
+            self.slug += '-' + str(uuid.uuid4()).split()[-1]
+        super().save(*args, force_insert=force_insert, force_update=force_update, using=using,
+                     update_fields=update_fields)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+
+class Product(BaseCreatedModel):
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, related_name='products', db_index=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    sale = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    count = models.IntegerField(validators=[MinValueValidator(1)])
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
