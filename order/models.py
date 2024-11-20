f80d9e4e27828500b372ad136baf947675df7bcc
ibrohim0117 <ibrohim.dev.uz@gmail.com>
2024-11-20 23:48:19 +0500

    .

--- order/models.py
+++ order/models.py
@@ -1,3 +1,28 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+from user.models import BaseCreatedModel
+
+
+class Order(BaseCreatedModel):
+
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='orders')
+    product = models.ForeignKey('product.Product', on_delete=models.CASCADE, related_name='orders')
+    quantity = models.IntegerField(validators=[MinValueValidator(1)])
+    status = models.CharField(max_length=20, choices=[
+            ('pending', 'Pending'),
+            ('completed', 'Completed'),
+            ('canceled', 'Canceled')
+        ],
+        default='pending'
+    )
+
+    @property
+    def total_price(self):
+        self.product.price * self.quantity
+
+    def __str__(self):
+        return self.product.name
+
+
+
